49d97bbfb6ffacaf7412c4e8017f1309c841ba49
Murphy McCauley <murphy.mccauley@gmail.com>
2023-05-24 11:41:58 -0400

    Switch to more standard palette
    We keep the old palette file in case you want to switch back!

---
+++ palette-vim-hybrid.h
@@ -0,0 +1,71 @@
+/*
+
+Made with https://terminal.sexy
+
+Here it is in Termite form for import:
+
+[colors]
+
+# special
+foreground      = #b8bbb9
+foreground_bold = #b8bbb9
+cursor          = #b8bbb9
+background      = #1d1f21
+
+# black
+color0  = #282a2e
+color8  = #373b41
+
+# red
+color1  = #a54242
+color9  = #cc6666
+
+# green
+color2  = #8c9440
+color10 = #b5bd68
+
+# yellow
+color3  = #de935f
+color11 = #f0c674
+
+# blue
+color4  = #5f819d
+color12 = #81a2be
+
+# magenta
+color5  = #85678f
+color13 = #b294bb
+
+# cyan
+color6  = #5e8d87
+color14 = #8abeb7
+
+# white
+color7  = #707880
+color15 = #c5c8c6
+
+*/
+
+uint32_t palette_raw[] = {
+0x1d1f21, //0x282a2e,
+0xa54242,
+0x8c9440,
+0xde935f,
+0x5f819d, //0x3f51ad
+0x85678f,
+0x5e8d87,
+0x707880,
+0x2d3032, //0x373b41, // Black
+0xcc6666,
+0xb5bd68,
+0xf0c674,
+0x81a2be,
+0xb294bb,
+0x8abeb7,
+0xc5c8c6,
+};
+
+uint32_t * bgpalette_raw = palette_raw;
+
+// You could also define eight separate colors for the background...
+//uint32_t bgpalette_raw[] = {...};
--- palette.h
+++ palette.h
@@ -1,68 +1,26 @@
 /*
 
-Made with https://terminal.sexy
-
-Here it is in Termite form for import:
-
-[colors]
-
-# special
-foreground      = #b8bbb9
-foreground_bold = #b8bbb9
-cursor          = #b8bbb9
-background      = #1d1f21
-
-# black
-color0  = #282a2e
-color8  = #373b41
-
-# red
-color1  = #a54242
-color9  = #cc6666
-
-# green
-color2  = #8c9440
-color10 = #b5bd68
-
-# yellow
-color3  = #de935f
-color11 = #f0c674
-
-# blue
-color4  = #5f819d
-color12 = #81a2be
-
-# magenta
-color5  = #85678f
-color13 = #b294bb
-
-# cyan
-color6  = #5e8d87
-color14 = #8abeb7
-
-# white
-color7  = #707880
-color15 = #c5c8c6
+Made with https://ciembor.github.io/4bit
 
 */
 
 uint32_t palette_raw[] = {
-0x1d1f21, //0x282a2e,
-0xa54242,
-0x8c9440,
-0xde935f,
-0x5f819d, //0x3f51ad
-0x85678f,
-0x5e8d87,
-0x707880,
-0x2d3032, //0x373b41, // Black
-0xcc6666,
-0xb5bd68,
-0xf0c674,
-0x81a2be,
-0xb294bb,
-0x8abeb7,
-0xc5c8c6,
+  0x171717,
+  0xa8203d,
+  0x3da820,
+  0xa88a20,
+  0x203da8,
+  0x8a20a8,
+  0x20a88a,
+  0xbdbdbd,
+  0x3b3b3b,
+  0xeb92a5,
+  0xa5eb92,
+  0xebd792,
+  0x92a5eb,
+  0xd792eb,
+  0x92ebd7,
+  0xf1f1f1,
 };
 
 uint32_t * bgpalette_raw = palette_raw;
